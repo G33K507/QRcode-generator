489022895d9bd7c7b33c3581dba68fbfe34e78c0
G33K507 <debtanusinharoy007@gmail.com>
2017-12-30 22:07:19 +0530

    Update QRcode_gen.py

--- QRcode_gen.py
+++ QRcode_gen.py
@@ -1,29 +1,26 @@
 from Tkinter import *
 import pyqrcode
 import png
-
+#----The Window----#
 mainWin = Tk()
-
-mainWin.minsize(300,100)
-
-label1 = Label(mainWin, text="--Input--", fg="aquamarine", bg="darkslategray")
-label1.pack()
-
-mainWin.configure(background="darkslategray")
-
 mainWin.title('QRcodeGen')
-
+mainWin.minsize(300,100)
+mainWin.configure(background="darkslategray")
+#----Custom Function For Generating The QRcode----#
 def qcode():
     global e
     string = e.get()
     img = pyqrcode.create(string)
     img.show(img)
-
+#----The Label----#
+label1 = Label(mainWin, text="--Input--", fg="aquamarine", bg="darkslategray")
+label1.pack()
+#----The Entry Field---#
 e = Entry(mainWin)
 e.pack()
 e.focus_set()
-
+#----The Button----#
 b = Button(mainWin,text='Generate!',command=qcode, bg="gray")
 b.pack(side='top')
-mainWin.mainloop()
 
+mainWin.mainloop()
